7a57e673f25cb84e857cb653e87d978e6c446798
Zsingor <3395224649@qq.com>
2024-08-27 14:58:58 +0800

    更新

--- ProjectWeb/src/router/permission.js
+++ ProjectWeb/src/router/permission.js
@@ -1,20 +1,18 @@
 import router from "@/router/index.js"
-import {userInfo} from "@/layout/user.js";
 import nprogress from "@/plugins/nprogress.js";
 
-const whiteList=['/login']
+const whiteList = ['/login']
 
 // 导航守卫
 // 使用 router.beforeEach 注册一个全局前置守卫，判断用户是否登陆
 router.beforeEach((to, from, next) => {
     nprogress.start()
     //白名单
-    if(whiteList.includes((to.path)))
-    {
+    if (whiteList.includes((to.path))) {
         next();
     }
-    let hasList=router.getRoutes()
-    if(hasList.some(item => item.path === to.path)){
+    let hasList = router.getRoutes()
+    if (hasList.some(item => item.path === to.path)) {
         next();
     }
     else {
