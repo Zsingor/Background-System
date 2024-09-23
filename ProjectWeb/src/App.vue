bd71fc1648b0fdab7a190c1e4f6e063abe322179
Zsingor <3395224649@qq.com>
2024-09-23 10:47:19 +0800

    优化watch监听

--- ProjectWeb/src/App.vue
+++ ProjectWeb/src/App.vue
@@ -23,24 +23,18 @@
 <script setup>
 import { persistentConfig, windowConfig } from '@/layout/layout.js'
 import { i18nChangeLanguage } from '@wangeditor/editor'
-import {
-  computed,
-  onMounted,
-  ref,
-  watchEffect,
-  watch,
-  getCurrentInstance,
-} from 'vue'
+import { onMounted, ref, watch, getCurrentInstance } from 'vue'
 import zhCn from '@/plugins/language/zh-cn.js'
 import en from '@/plugins/language/en.js'
 
 const { proxy } = getCurrentInstance()
+//用于文档样式的变化
+const el = document.documentElement
 
-//const language = ref('zh-cn')
-//const locale = computed(() => (language.value === 'zh-cn' ? zhCn : en))
-let locale=ref(zhCn)
+let locale = ref(zhCn)
 let language = ref('zh-CN')
 
+//根据窗口大小改变设置
 const onResize = () => {
   window.innerWidth > 1024
     ? (windowConfig.isPc = true)
@@ -57,63 +51,64 @@ const onResize = () => {
   }
 }
 
-onResize()
-
-// 开始监听persistentConfig，当属性发生改变时同步到localStorage进行持久化
-watch(persistentConfig, (value) => {
-  localStorage.setItem('Global_Config', JSON.stringify(value))
-})
-
-// 监听全局语言的选择，改变系统语言
-watch(()=>persistentConfig.localeLang, (value) => {
-  proxy.$i18n.locale = value
-  //改变element-plus的国际化语言
-  locale.value=value==="zhCn"? zhCn : en
-  //改变wangEditor的国际化语言
-  language.value=value==="zhCn"?"zh-CN":"en"
-  i18nChangeLanguage(language.value)
-})
-
 //初始化系统语言
-const languageInit=()=>{
-  locale.value=persistentConfig.localeLang==="zhCn"? zhCn : en
-  language.value=persistentConfig.localeLang==="zhCn"?"zh-CN":"en"
+const languageInit = () => {
+  locale.value = persistentConfig.localeLang === 'zhCn' ? zhCn : en
+  language.value = persistentConfig.localeLang === 'zhCn' ? 'zh-CN' : 'en'
   i18nChangeLanguage(language.value)
 }
 
+// 开始监听persistentConfig，当属性发生改变时持久化存储并同步更改相应的配置
+watch(persistentConfig, (value) => {
+  //持久化存储全局设置
+  localStorage.setItem('Global_Config', JSON.stringify(value))
+
+  // 监听滚动条变化，仅限非手机尺寸
+  if (windowConfig.breakpoint !== 'xs') {
+    //是否使用自定义滚动条
+    if (persistentConfig.scroll.openCustom) {
+      el.classList.add('app-scroll')
+    } else {
+      el.classList.remove('app-scroll')
+    }
+    //根据滚动条配置改变样式
+    switch (persistentConfig.scroll.size) {
+      case 'large':
+        el.style.setProperty('--scroll-size', '18px')
+        break
+      case 'medium':
+        el.style.setProperty('--scroll-size', '14px')
+        break
+      case 'small':
+        el.style.setProperty('--scroll-size', '10px')
+        break
+      case 'hide':
+        el.style.setProperty('--scroll-size', '0px')
+        break
+    }
+  }
+})
+
+// 监听全局语言的选择，改变系统语言
+watch(
+  () => persistentConfig.localeLang,
+  (value) => {
+    proxy.$i18n.locale = value
+    //改变element-plus的国际化语言
+    locale.value = value === 'zhCn' ? zhCn : en
+    //改变wangEditor的国际化语言
+    language.value = value === 'zhCn' ? 'zh-CN' : 'en'
+    i18nChangeLanguage(language.value)
+  }
+)
+
 onMounted(() => {
+  onResize()
+
   languageInit()
 
   // 开启窗口变化监听
   window.addEventListener('resize', onResize, { passive: true })
-
-  // 监听滚动条变化，仅限非手机尺寸
-  const el = document.documentElement
-  if (windowConfig.breakpoint !== 'xs') {
-    watchEffect(() => {
-      if (persistentConfig.scroll.openCustom) {
-        el.classList.add('app-scroll')
-      } else {
-        el.classList.remove('app-scroll')
-      }
-    })
-    watchEffect(() => {
-      switch (persistentConfig.scroll.size) {
-        case 'large':
-          el.style.setProperty('--scroll-size', '18px')
-          break
-        case 'medium':
-          el.style.setProperty('--scroll-size', '14px')
-          break
-        case 'small':
-          el.style.setProperty('--scroll-size', '10px')
-          break
-        case 'hide':
-          el.style.setProperty('--scroll-size', '0px')
-          break
-      }
-    })
-  }
 })
 </script>
 
